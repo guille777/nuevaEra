f137f1d31fa1fedf68929e2300570e8a3365505b
guille777 <gbrfloridaa@gmail.com>
2017-03-22 12:22:23 +0100

    C.R.U.D DataBase

---
+++ 23-crud/inder.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="test.css">
+  </head>
+  <body>
+  <!required,placeholder...  name  $_POST || $_GET      redireccionar todo a index.php s pued">
+  <section>
+    <article class="medio">
+      <form class="" action="insertarDB.php" method="post">
+        <h1>Inserta un Equipo</h1>
+        <input type="text" name="nombre" value="" placeholder="Introduce un Nombre"><br><br>
+        <input type="text" name="ciudad" value="" placeholder="Introduce una Ciudad"><br><br>
+        <input type="text" name="conferencia" value="" placeholder="Introduce una Conferencia"><br><br>
+        <input type="text" name="division" value="" placeholder="Introduce una Division"><br><br>
+        <input type="submit" name="" value="insertar">
+      </form>
+    </article>
+  </section>
+  </body>
+</html>
